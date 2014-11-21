1f1390fb1b69b55d927a906576c0b9ce3e85bf3c
tugberkugurlu <tugberkugurlu@gmail.com>
2014-11-21 15:50:10 +0200

    intial working implementation without docs and instructions

---
+++ BitToGithub/BitBuckect.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace BitToGithub
+{
+    public class BitBuckect
+    {
+        public class Rootobject
+        {
+            public int count { get; set; }
+            public Filter filter { get; set; }
+            public object search { get; set; }
+            public Issue[] issues { get; set; }
+        }
+
+        public class Filter
+        {
+        }
+
+        public class Issue
+        {
+            public string status { get; set; }
+            public string priority { get; set; }
+            public string title { get; set; }
+            public Reported_By reported_by { get; set; }
+            public string utc_last_updated { get; set; }
+            public Responsible responsible { get; set; }
+            public DateTime created_on { get; set; }
+            public Metadata metadata { get; set; }
+            public string content { get; set; }
+            public int comment_count { get; set; }
+            public int local_id { get; set; }
+            public int follower_count { get; set; }
+            public string utc_created_on { get; set; }
+            public string resource_uri { get; set; }
+            public bool is_spam { get; set; }
+        }
+
+        public class Reported_By
+        {
+            public string username { get; set; }
+            public string first_name { get; set; }
+            public string last_name { get; set; }
+            public string display_name { get; set; }
+            public bool is_staff { get; set; }
+            public string avatar { get; set; }
+            public string resource_uri { get; set; }
+            public bool is_team { get; set; }
+        }
+
+        public class Responsible
+        {
+            public string username { get; set; }
+            public string first_name { get; set; }
+            public string last_name { get; set; }
+            public string display_name { get; set; }
+            public bool is_staff { get; set; }
+            public string avatar { get; set; }
+            public string resource_uri { get; set; }
+            public bool is_team { get; set; }
+        }
+
+        public class Metadata
+        {
+            public string kind { get; set; }
+            public string version { get; set; }
+            public string component { get; set; }
+            public string milestone { get; set; }
+        }
+
+    }
+
+    public class BitBucketComment
+    {
+        public class Rootobject
+        {
+            public Class1[] Property1 { get; set; }
+        }
+
+        public class Class1
+        {
+            public string content { get; set; }
+            public Author_Info author_info { get; set; }
+            public int comment_id { get; set; }
+            public string utc_updated_on { get; set; }
+            public bool convert_markup { get; set; }
+            public string utc_created_on { get; set; }
+            public bool is_spam { get; set; }
+        }
+
+        public class Author_Info
+        {
+            public string username { get; set; }
+            public string first_name { get; set; }
+            public string last_name { get; set; }
+            public string display_name { get; set; }
+            public bool is_staff { get; set; }
+            public string avatar { get; set; }
+            public string resource_uri { get; set; }
+            public bool is_team { get; set; }
+        }
+
+    }
+}
---
+++ BitToGithub/Program.cs
@@ -0,0 +1,135 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace BitToGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string bbUsername = "";
+            const string bbPassword = "";
+            const string bbRepoName = "";
+            const string githubUsername = "";
+            const string githubPassword = "";
+            const string githubRepoName = "";
+            const string githubRepoOwnerName = "";
+
+            var isAllDefined = new[] { bbUsername, bbPassword, bbRepoName, githubUsername, githubPassword, githubRepoName, githubRepoOwnerName }.All(param => string.IsNullOrEmpty(param) == false);
+            if (isAllDefined)
+            {
+                string bbRepoIssuesUrl = "https://api.bitbucket.org/1.0/repositories/\{bbUsername}/\{bbRepoName}/issues";
+
+                var github = new GitHubClient(new Octokit.ProductHeaderValue("MyAmazingApp"));
+                github.Credentials = new Credentials(githubUsername, githubPassword);
+                var repo = github.Repository.Get(githubRepoOwnerName, githubRepoName).Result;
+
+                using (HttpClient client = new HttpClient())
+                {
+                    const string authHeaderRaw = bbUsername + ":" + bbPassword;
+                    const string migratedLabel = "bitbucket-migrated";
+                    AuthenticationHeaderValue authHeader = new AuthenticationHeaderValue("Basic", EncodeToBase64(authHeaderRaw));
+                    client.DefaultRequestHeaders.Authorization = authHeader;
+
+                    // create the migration label.
+                    try
+                    {
+                        github.Issue.Labels.Get(githubRepoOwnerName, githubRepoName, migratedLabel).Wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        // assume that the label is not there. create.
+                        github.Issue.Labels.Create(githubRepoOwnerName, githubRepoName, new NewLabel(migratedLabel, "fef2c0")).Wait();
+                    }
+
+                    int startIndex = 1;
+                    while (true)
+                    {
+                        var response = client.GetAsync(bbRepoIssuesUrl + "?start=\{startIndex}").Result;
+                        response.EnsureSuccessStatusCode();
+                        var result = response.Content.ReadAsAsync<BitBuckect.Rootobject>().Result;
+                        startIndex += result.issues.Length;
+                        if (result.issues.Length > 0)
+                        {
+                            foreach (var issue in result.issues)
+                            {
+                                Console.WriteLine("found issue {0}", issue.local_id);
+                                var newIssue = new NewIssue(issue.title);
+                                newIssue.Labels.Add(migratedLabel);
+                                newIssue.Body = issue.content + "\{Environment.NewLine}\{Environment.NewLine}> Originally created at \{issue.utc_created_on} (UTC) by \{issue.reported_by.username} as a(n) \{issue.priority} issue.";
+
+                                if (string.IsNullOrWhiteSpace(issue.metadata.component) == false)
+                                {
+                                    // check label
+                                    try
+                                    {
+                                        github.Issue.Labels.Get(githubRepoOwnerName, githubRepoName, issue.metadata.component).Wait();
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        // assume that the label is not there. create.
+                                        Console.WriteLine("Creating label {0}", issue.metadata.component);
+                                        github.Issue.Labels.Create(githubRepoOwnerName, githubRepoName, new NewLabel(issue.metadata.component, "d4c5f9")).Wait();
+                                    }
+
+                                    newIssue.Labels.Add(issue.metadata.component);
+                                }
+
+                                // push the issue to GitHub
+                                Console.WriteLine("Creating issue {0}", issue.local_id);
+                                var createdIssue = github.Issue.Create(githubRepoOwnerName, githubRepoName, newIssue).Result;
+                                Console.WriteLine("Create issue {0} on github.", createdIssue.Number);
+
+                                if (issue.status.Equals("resolved", StringComparison.InvariantCultureIgnoreCase))
+                                {
+                                    Console.WriteLine("Closing issue {0} on github.", createdIssue.Number);
+                                    github.Issue.Update(githubRepoOwnerName, githubRepoName, createdIssue.Number, new IssueUpdate
+                                    {
+                                        State = ItemState.Closed
+                                    }).Wait();
+                                }
+
+                                if (issue.comment_count > 0)
+                                {
+                                    var commentsResponse = client.GetAsync(bbRepoIssuesUrl + "/\{issue.local_id}/comments").Result;
+                                    if (commentsResponse.IsSuccessStatusCode)
+                                    {
+                                        var comments = commentsResponse.Content.ReadAsAsync<IEnumerable<BitBucketComment.Class1>>().Result;
+                                        foreach (var comment in comments)
+                                        {
+                                            Console.WriteLine("Adding comment {0} for github issue {1}.", comment.comment_id, createdIssue.Number);
+                                            github.Issue.Comment.Create(githubRepoOwnerName, githubRepoName, createdIssue.Number, comment.content).Wait();
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Failed to fetch comments for issye {0}", issue.local_id);
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Set all the necessary constant variables.");
+            }
+        }
+
+        private static string EncodeToBase64(string value)
+        {
+            byte[] toEncodeAsBytes = Encoding.UTF8.GetBytes(value);
+            return Convert.ToBase64String(toEncodeAsBytes);
+        }
+    }
+}
