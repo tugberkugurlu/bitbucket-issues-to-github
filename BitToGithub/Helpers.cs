ac85b03a8302993081e6fef35c740620770af647
Tim Long <Tim@tigranetworks.co.uk>
2019-09-06 08:03:28 +0100

    Added command line options based on CommandLine library. Updated to use Bitbucket API 2.0, which has different data models. Restructured the code to better work with paged datasets and command line options.

---
+++ BitToGithub/Helpers.cs
@@ -0,0 +1,50 @@
+﻿// This file is part of the BitToGithub project
+//
+// Copyright © 2016-2019 Tigra Astronomy, all rights reserved.
+//
+// File: Helpers.cs  Last modified: 2019-09-06@03:52 by Tim Long
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BitToGithub
+{
+    internal static class Helpers
+    {
+        public static string Base64Encoded(this string value)
+        {
+            byte[] toEncodeAsBytes = Encoding.UTF8.GetBytes(value);
+            return Convert.ToBase64String(toEncodeAsBytes);
+        }
+
+        public static void WithErrors(this ExitCode code, IEnumerable<string> errors = null)
+        {
+            var errorsToReport = errors.ToList();
+            switch (code)
+            {
+                case ExitCode.SignalSuccess:
+                    Console.WriteLine("Completed successfully");
+                    break;
+                default:
+                    if (errorsToReport.Any())
+                    {
+                        Console.WriteLine($"Exiting with code {(int) code} ({code}) due to the following errors:");
+                        foreach (var error in errors)
+                        {
+                            Console.WriteLine(error);
+                        }
+                    }
+                    else
+                        Console.WriteLine($"Exiting with code {(int) code} ({code})");
+                    break;
+            }
+#if DEBUG
+            Console.WriteLine("Press <ENTER> to close the program...");
+            Console.ReadLine();
+#endif //DEBUG
+            Environment.Exit((int) code);
+        }
+    }
+}
--- BitToGithub/Program.cs
+++ BitToGithub/Program.cs
@@ -1,145 +1,79 @@
-﻿using Octokit;
+﻿// This file is part of the BitToGithub project
+//
+// Copyright © 2016-2019 Tigra Astronomy, all rights reserved.
+//
+// File: Program.cs  Last modified: 2019-09-06@02:44 by Tim Long
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
 
 namespace BitToGithub
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            const string bbUsername = "";
-            const string bbPassword = "";
-            const string bbRepoName = "";
-            const string bbRepoOwnerName = "";
-
-            const string githubUsername = "";
-            const string githubPassword = "";
-            const string githubRepoName = "";
-            const string githubRepoOwnerName = "";
-
-            var isAllDefined = new[] { bbUsername, bbPassword, bbRepoName, githubUsername, githubPassword, githubRepoName, githubRepoOwnerName }.All(param => string.IsNullOrEmpty(param) == false);
-            if (isAllDefined)
-            {
-                string bbRepoIssuesUrl = string.Format("https://api.bitbucket.org/1.0/repositories/{0}/{1}/issues", bbRepoOwnerName, bbRepoName);
-
-                var github = new GitHubClient(new Octokit.ProductHeaderValue("MyAmazingApp"));
-                github.Credentials = new Credentials(githubUsername, githubPassword);
-                var repo = github.Repository.Get(githubRepoOwnerName, githubRepoName).Result;
-
-                using (HttpClient client = new HttpClient())
+            PrintBanner();
+            var commandLineParser = new Parser(with =>
                 {
-                    const string authHeaderRaw = bbUsername + ":" + bbPassword;
-                    const string migratedLabel = "bitbucket-migrated";
-                    AuthenticationHeaderValue authHeader = new AuthenticationHeaderValue("Basic", EncodeToBase64(authHeaderRaw));
-                    client.DefaultRequestHeaders.Authorization = authHeader;
+                    with.CaseSensitive = true;
+                    with.IgnoreUnknownArguments = false;
+                    with.HelpWriter = Console.Out;
+                    with.AutoVersion = true;
+                    with.AutoHelp = true;
+                });
 
-                    // create the migration label.
-                    try
-                    {
-                        github.Issue.Labels.Get(githubRepoOwnerName, githubRepoName, migratedLabel).Wait();
-                    }
-                    catch (Exception ex)
-                    {
-                        // assume that the label is not there. create.
-                        github.Issue.Labels.Create(githubRepoOwnerName, githubRepoName, new NewLabel(migratedLabel, "fef2c0")).Wait();
-                    }
+            var errorMessages = new List<string>();
+            MigrationOptions parsedOptions = null;
 
-                    int startIndex = 1;
-                    while (true)
-                    {
-                        var response = client.GetAsync(bbRepoIssuesUrl + "?start=" + startIndex.ToString()).Result;
-                        response.EnsureSuccessStatusCode();
-                        var result = response.Content.ReadAsAsync<BitBuckect.Rootobject>().Result;
-                        startIndex += result.issues.Length;
-                        if (result.issues.Length > 0)
-                        {
-                            foreach (var issue in result.issues)
-                            {
-                                Console.WriteLine("found issue {0}", issue.local_id);
-                                var newIssue = new NewIssue(issue.title);
-                                newIssue.Labels.Add(migratedLabel);
-
-                                newIssue.Body =
-                                    issue.content +
-                                    Environment.NewLine +
-                                    Environment.NewLine +
-                                    String.Format("> Originally created at {0} (UTC) by {1} as a(n) {2} issue.", issue.utc_created_on, issue.reported_by.username, issue.priority);
-
-                                if (string.IsNullOrWhiteSpace(issue.metadata.component) == false)
-                                {
-                                    // check label
-                                    try
-                                    {
-                                        github.Issue.Labels.Get(githubRepoOwnerName, githubRepoName, issue.metadata.component).Wait();
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        // assume that the label is not there. create.
-                                        Console.WriteLine("Creating label {0}", issue.metadata.component);
-                                        github.Issue.Labels.Create(githubRepoOwnerName, githubRepoName, new NewLabel(issue.metadata.component, "d4c5f9")).Wait();
-                                    }
-
-                                    newIssue.Labels.Add(issue.metadata.component);
-                                }
-
-                                // push the issue to GitHub
-                                Console.WriteLine("Creating issue {0}", issue.local_id);
-                                var createdIssue = github.Issue.Create(githubRepoOwnerName, githubRepoName, newIssue).Result;
-                                Console.WriteLine("Create issue {0} on github.", createdIssue.Number);
-
-                                if (issue.status.Equals("resolved", StringComparison.InvariantCultureIgnoreCase))
-                                {
-                                    Console.WriteLine("Closing issue {0} on github.", createdIssue.Number);
-                                    github.Issue.Update(githubRepoOwnerName, githubRepoName, createdIssue.Number, new IssueUpdate
-                                    {
-                                        State = ItemState.Closed
-                                    }).Wait();
-                                }
-
-                                if (issue.comment_count > 0)
-                                {
-                                    var commentsResponse = client.GetAsync(bbRepoIssuesUrl + "/" + issue.local_id + "/comments").Result;
-
-                                    if (commentsResponse.IsSuccessStatusCode)
-                                    {
-                                        var comments = commentsResponse.Content.ReadAsAsync<IEnumerable<BitBucketComment.Class1>>().Result;
-                                        foreach (var comment in comments)
-                                        {
-                                            Console.WriteLine("Adding comment {0} for github issue {1}.", comment.comment_id, createdIssue.Number);
-                                            github.Issue.Comment.Create(githubRepoOwnerName, githubRepoName, createdIssue.Number, comment.content).Wait();
-                                        }
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Failed to fetch comments for issue {0}", issue.local_id);
-                                    }
-                                }
-                                System.Threading.Thread.Sleep(3000);
-                            }
-                        }
-                        else
-                        {
-                            Console.ReadLine();
-                            break;
-                        }
-                    }
-                }
-            }
-            else
+            try
             {
-                Console.WriteLine("Set all the necessary constant variables.");
+                var result = commandLineParser.ParseArguments<MigrationOptions>(args);
+                result
+                    .WithParsed(options => parsedOptions = options)
+                    .WithNotParsed(errors => errors.ToList().ForEach(e => errorMessages.Add(e.ToString())));
+            }
+            catch (Exception ex)
+            {
+                errorMessages.Add(ex.Message);
+            }
+
+            if (errorMessages.Any())
+                ExitCode.SignalInvalidOptions.WithErrors(errorMessages);
+
+            try
+            {
+                var migrator = new IssueMigrator(parsedOptions);
+                var exitCode = await migrator.Run();
+                exitCode.WithErrors(Enumerable.Empty<string>());
+            }
+            catch (AggregateException ae)
+            {
+                foreach (var innerException in ae.InnerExceptions)
+                {
+                    errorMessages.Add(innerException.Message);
+                }
+                ExitCode.SignalMigrationFailed.WithErrors(errorMessages);
+            }
+            catch (HttpRequestException e)
+            {
+                errorMessages.Add($"Bitbucket error: {e.Message}");
+                ExitCode.SignalMigrationFailed.WithErrors(errorMessages);
+            }
+            catch (Exception e)
+            {
+                errorMessages.Add(e.Message);
+                ExitCode.SignalMigrationFailed.WithErrors(errorMessages);
             }
         }
 
-        private static string EncodeToBase64(string value)
+        private static void PrintBanner()
         {
-            byte[] toEncodeAsBytes = Encoding.UTF8.GetBytes(value);
-            return Convert.ToBase64String(toEncodeAsBytes);
+            Console.WriteLine("Bitbucket to GitHub Issue Migrator");
         }
     }
 }
